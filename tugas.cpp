1fd663f18e337c0a3db63ebfc5883417e8022ebf
khansa-qonita <khansaqonita420@gmail.com>
2026-03-10 18:45:02 +0700

    menambahkan prosedur untuk menambahkan berat & tinggi

--- tugas.cpp
+++ tugas.cpp
@@ -2,4 +2,9 @@
 #include <string>
 using namespace std;
 
-
+void inputData(float berat, float tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
